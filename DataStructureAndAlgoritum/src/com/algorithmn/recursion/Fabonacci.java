378d72220c97b795d63c327a047b352e6f174448
Suraj Hase <95306606+surajhase@users.noreply.github.com>
2021-12-21 22:34:08 +0530

    Refactr changes

--- DataStructureAndAlgoritum/src/com/algorithmn/recursion/Fabonacci.java
+++ DataStructureAndAlgoritum/src/com/algorithmn/recursion/Fabonacci.java
@@ -1,31 +1,11 @@
 package com.algorithmn.recursion;
 
-public class RecurrsionsSmallPrograms {
+public class Fabonacci {
 
 	public static void main(String[] args) {
-		RecurrsionsSmallPrograms recurrsionsSmallPrograms = new RecurrsionsSmallPrograms();
-		System.out.println(recurrsionsSmallPrograms.getFactorialUsingIteration(5));
-		System.out.println(recurrsionsSmallPrograms.getFactorialUsingRecursion(5));
-		System.out.println(recurrsionsSmallPrograms.getFabonacciUsingIteration(11));
-		System.out.println(recurrsionsSmallPrograms.getFabonacciUsingRecursion(8));
-	}
-
-//	O(n)
-	private long getFactorialUsingRecursion(int number) {
-		long result = number;
-		if (number > 1) {
-			result = number * getFactorialUsingRecursion(--number);
-		}
-		return result;
-	}
-
-//	O(n)
-	private long getFactorialUsingIteration(int number) {
-		long result = 1;
-		for (int i = 2; i <= number; i++) {
-			result = result * i;
-		}
-		return result;
+		Fabonacci fabonacci = new Fabonacci();
+		System.out.println(fabonacci.getFabonacciUsingIteration(11));
+		System.out.println(fabonacci.getFabonacciUsingRecursion(8));
 	}
 
 	// Given the number N return the index value of the fibonacci sequence
@@ -33,8 +13,8 @@ public class RecurrsionsSmallPrograms {
 	// index: 0 1 2 3 4 5 6 7 8 9 10 11
 	// So in case the input number is 6 then it should return 8
 	// the input number is 8 then it should return 21
-//	O(2^n)
-	private long getFabonacciUsingRecursion(int number) {
+//		O(2^n)
+	public long getFabonacciUsingRecursion(int number) {
 		if (number < 2) {
 			return number;
 		}
@@ -47,8 +27,8 @@ public class RecurrsionsSmallPrograms {
 	// index: 0 1 2 3 4 5 6 7 8 9 10 11
 	// So in case the input number is 6 then it should return 8
 	// the input number is 8 then it should return 21
-//	O(n)
-	private long getFabonacciUsingIteration(int number) {
+//		O(n)
+	public long getFabonacciUsingIteration(int number) {
 		long nMinusOneIndex = 0;
 		long result = 1;
 		for (int i = 2; i <= number; i++) {
---
+++ DataStructureAndAlgoritum/src/com/algorithmn/recursion/Factorial.java
@@ -0,0 +1,27 @@
+package com.algorithmn.recursion;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Factorial factorial = new Factorial();
+		System.out.println(factorial.getFactorialUsingIteration(5));
+		System.out.println(factorial.getFactorialUsingRecursion(5));
+	}
+
+//	O(n)
+	private long getFactorialUsingRecursion(int number) {
+		long result = number;
+		if (number > 1) {
+			result = number * getFactorialUsingRecursion(--number);
+		}
+		return result;
+	}
+
+//	O(n)
+	private long getFactorialUsingIteration(int number) {
+		long result = 1;
+		for (int i = 2; i <= number; i++) {
+			result = result * i;
+		}
+		return result;
+	}
+}
